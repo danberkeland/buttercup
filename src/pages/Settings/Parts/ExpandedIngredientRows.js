16a490daac169f2a0c80f97eb53b4276ec878f6a
Dan Berkeland <danberkeland@gmail.com>
2021-04-23 13:43:20 -0700

    incomplete ingredient add on

--- src/pages/Settings/Parts/ExpandedIngredientRows.js
+++ src/pages/Settings/Parts/ExpandedIngredientRows.js
@@ -13,6 +13,14 @@ export const ExpandedIngredientRows = ({ data, bakeryItems, setBakeryItems }) =>
     return <ExpandedIngredientDetailRows data={e} bakeryItems={bakeryItems} setBakeryItems={setBakeryItems}/>;
   };
 
+  const createIngName = (e) => {
+    let isComplete =""
+    if (e.par===0 || e.actionType==='' || e.updateList===''){
+      isComplete =" (incomplete)"
+    }
+    return e.ingName+isComplete
+  }
+
   return (
     <div className="delivDate-subtable">
       <DataTable
@@ -26,7 +34,7 @@ export const ExpandedIngredientRows = ({ data, bakeryItems, setBakeryItems }) =>
         <Column headerStyle={{ width: "4rem" }}></Column>
         <Column expander style={{ width: "3em" }} />
 
-        <Column field="ingName" header="Ingredient" />
+        <Column field="ingName" header="Ingredient" body={e => createIngName(e)}/>
         <Column
             headerStyle={{ width: "4rem" }}
             body={(e) => DeleteIngredient(e.ingName,data.locationName,bakeryItems,setBakeryItems)}
