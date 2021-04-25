6675a7b19daa0062d84cc273689f8a003015fe72
Dan Berkeland <danberkeland@gmail.com>
2021-04-25 14:00:55 -0700

    added who updated info to OrderList

--- src/pages/OrderList/Parts/ExpandedVendorRows.js
+++ src/pages/OrderList/Parts/ExpandedVendorRows.js
@@ -21,7 +21,8 @@ function ExpandedVendorRows({
     <div>
       {ingList.map((ing) => (
         <React.Fragment>
-          <h4>{ing.ingName}:{Number(ing.par) - Number(ing.actual)}</h4>
+          <h2>{ing.ingName}:{Number(ing.par) - Number(ing.actual)}</h2>
+          <h5>Last Updated: {ing.updatedAt} by {ing.whoUpdatedLast}</h5>
           
         </React.Fragment>
       ))}
