5729fe6a98972c985604ccef93b56f07c3a33ca4
Dan Berkeland <danberkeland@gmail.com>
2021-04-25 13:24:39 -0700

    skeleton of order list

---
+++ src/pages/OrderList/Parts/ExpandedVendorRows.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function ExpandedVendorRows() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h2>ExpandedVendorRows</h2>
+      </header>
+    </div>
+  );
+}
+
+export default ExpandedVendorRows;
