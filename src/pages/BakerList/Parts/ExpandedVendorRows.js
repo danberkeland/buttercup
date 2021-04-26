f8cb5d1e6d4975d77a0d7d44802f0aff9a46adb5
Dan Berkeland <danberkeland@gmail.com>
2021-04-26 05:54:45 -0700

    All tabs working!

---
+++ src/pages/BakerList/Parts/ExpandedVendorRows.js
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from "react";
+import TimeAgo from "timeago-react"; // var TimeAgo = require('timeago-react');
+import us from "timeago.js/lib/lang/en_US";
+import { Checkbox } from "primereact/checkbox";
+import styled from "styled-components";
+
+import { updateBakeryItem } from "../../../graphql/mutations";
+
+import { API, graphqlOperation } from "aws-amplify";
+
+const clonedeep = require("lodash.clonedeep");
+
+const Ingredient = styled.div`
+  font-weight: bold;
+`;
+
+const IngDetails = styled.div`
+  font-size: 0.8em;
+`;
+
+const ListGrid = styled.div`
+  
+  display: grid;
+  justify-items:center;
+  align-items: flex-start;
+  grid-template-columns: 1fr 5fr;
+`;
+function ExpandedVendorRows({
+  data,
+  lists,
+  setLists,
+  bakeryItems,
+  setBakeryItems,
+  signedIn,
+}) {
+  const [ingList, setIngList] = useState([]);
+
+  useEffect(() => {
+    let ing = bakeryItems.filter((ba) => ba.actionType === data.vendorName);
+    setIngList(ing);
+  }, [bakeryItems]);
+
+  const updateDBattr = async (id, attr, val) => {
+    let addDetails = {
+      id: id,
+      isChecked: val,
+    };
+
+    console.log(addDetails);
+    try {
+      await API.graphql(
+        graphqlOperation(updateBakeryItem, { input: { ...addDetails } })
+      );
+    } catch (error) {
+      console.log("error on updating bakery items", error);
+    }
+  };
+
+  const handleChecked = (checked, ing) => {
+    console.log(checked);
+    let id = ing.ingName + ing.location;
+    console.log(id);
+    //try {
+    let listToModify = clonedeep(bakeryItems);
+    let ind = listToModify.findIndex((inv) => inv["id"] === id);
+    let val = checked;
+
+    listToModify[ind].isChecked = val;
+    updateDBattr(id, "isChecked", val);
+
+    setBakeryItems(listToModify);
+    //} catch {
+    //  console.log("error updating attribute.");
+    //}
+  };
+
+  return (
+    <div>
+      {ingList.map((ing) => (
+        <ListGrid>
+          <div>
+            <Checkbox
+              key={ing.id + "check"}
+              onChange={(e) => handleChecked(e.checked, ing)}
+              checked={ing.isChecked}
+            ></Checkbox>
+          </div>
+          <div
+            style={ing.isChecked ? { color: "lightgray" } : { color: "black" }}
+          >
+            <Ingredient key={ing.id + "ing"}>
+              {ing.ingName} &nbsp;({ing.location}) 
+            </Ingredient>
+            <IngDetails>
+              <div>
+                Counted &nbsp;
+                <TimeAgo
+                  key={ing.id + "time"}
+                  datetime={ing.updatedAt}
+                  locale={us}
+                />
+                &nbsp;by {ing.whoUpdatedLast}
+              </div>
+            </IngDetails>
+          </div>
+        </ListGrid>
+      ))}
+    </div>
+  );
+}
+
+export default ExpandedVendorRows;
---
+++ src/pages/FOHList/Parts/Vendors.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+
+import ExpandedVendorRows from "./ExpandedVendorRows";
+
+const { DateTime } = require("luxon");
+
+const Vendors = ({
+  lists,
+  setLists,
+  bakeryItems,
+  setBakeryItems,
+  signedIn,
+}) => {
+  const [expandedRows, setExpandedRows] = useState(null);
+
+  let yesterday = DateTime.now()
+    .setZone("America/Los_Angeles")
+    .minus({ days: 1 }).weekdayShort;
+  let todayLists = lists.filter((li) => li.listNeedDay.includes(yesterday));
+  todayLists = todayLists.map((to) => to.listAffect);
+
+  let vendors = Array.from(
+    new Set(
+      bakeryItems
+        .filter(
+          (ba) =>
+            ba.updateList === "FOH" && todayLists.includes(ba.actionType)
+        )
+        .map((ve) => ve.actionType)
+    )
+  ).map((ve) => ({
+    vendorName: ve,
+  }));
+
+  const rowExpansionTemplate = (data) => {
+    return (
+      <ExpandedVendorRows
+        data={data}
+        lists={lists}
+        setLists={setLists}
+        bakeryItems={bakeryItems}
+        setBakeryItems={setBakeryItems}
+      />
+    );
+  };
+
+  return (
+    <div className="datatable-rowexpansion-demo">
+      <div className="card">
+        <DataTable
+          value={vendors}
+          expandedRows={expandedRows}
+          onRowToggle={(e) => setExpandedRows(e.data)}
+          rowExpansionTemplate={(e) => rowExpansionTemplate(e)}
+          dataKey="locationName"
+          className="p-datatable-sm"
+        >
+          <Column expander style={{ width: "3em" }} />
+          <Column field="vendorName" header="What To Do" />
+        </DataTable>
+      </div>
+    </div>
+  );
+};
+
+export default Vendors;
